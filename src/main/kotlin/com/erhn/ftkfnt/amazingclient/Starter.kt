205ee750248ec31cdde81fe13c7065bdb6b94678
erhnftknft <fetakonfeta@gmail.com>
2020-09-27 14:08:58 +0300

    Add base dependecies.

---
+++ src/main/kotlin/com/erhn/ftkfnt/amazingclient/Starter.kt
@@ -0,0 +1,11 @@
+package com.erhn.ftkfnt.amazingclient
+
+import io.vertx.core.Vertx
+
+object Starter {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello world!")
+    }
+}
